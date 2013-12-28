2aaaf94ba1552c096cb25818c1817647df723bad
Bryan Ricker <bricker88@gmail.com>
2013-12-27 16:55:17 -0800

    Get build working again, add HTTP client for articles

--- KPCC/src/main/java/com/kpcc/android/ArticlesActivity.java
+++ KPCC/src/main/java/com/kpcc/android/ArticlesActivity.java
@@ -1,36 +1,66 @@
+package com.kpcc.android;
+
+import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class ArticlesActivity extends Activity {
-    private EditText urlText;
+    public final String DEBUG_TAG = "Articles";
+    public final String API_ROOT = "http://scpr.org/api/v3/";
+    public final String ENDPOINT = "articles";
+
     private TextView textView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_articles);
 
-        urlText     = (EditText) findViewById(R.id.myUrl);
-        textView    = (TextView) findViewById(R.id.myText);
+        textView = (TextView) findViewById(R.id.articles);
 
-        String stringUrl = "http://scpr.org/api/v3/articles"
         ConnectivityManager connMgr = (ConnectivityManager) 
             getSystemService(Context.CONNECTIVITY_SERVICE);
 
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected()) {
-            new DownloadWebpageTask().execute(stringUrl);
+            new FetchArticlesTask().execute(apiUrl(ENDPOINT));
         } else {
             textView.setText("No network connection available.");
         }
-
     }
 
 
 
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    private String apiUrl(String endpoint) {
+        return API_ROOT + endpoint;
+    }
+
+
+    private class FetchArticlesTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
             try {
-                return downloadUrl(urls[0]);
+                return fetchArticles(urls[0]);
             } catch (IOException e) {
                 return "Unable to retrieve web page. URL may be invalid.";
             }
@@ -39,5 +69,46 @@ public class ArticlesActivity extends Activity {
         @Override
         protected void onPostExecute(String result) {
             textView.setText(result);
+        }
+
+
+        public String readIt(InputStream stream, int len)
+        throws IOException, UnsupportedEncodingException {
+           Reader reader = null;
+           reader = new InputStreamReader(stream, "UTF-8");
+
+           char[] buffer = new char[len];
+           reader.read(buffer);
+           return new String(buffer);
        }
+
+
+       private String fetchArticles(String strUrl) throws IOException {
+           InputStream is = null;
+
+           try {
+               URL url = new URL(strUrl);
+
+               HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+               conn.setReadTimeout(10000);
+               conn.setConnectTimeout(15000);
+               conn.setRequestMethod("GET");
+               conn.setDoInput(true);
+
+               conn.connect();
+               int response = conn.getResponseCode();
+               Log.d(DEBUG_TAG, "The response is: " + response);
+               is = conn.getInputStream();
+
+               String contentAsString = readIt(is, 999);
+               return contentAsString;
+
+           } finally {
+               if (is != null) {
+                   is.close();
+               } 
+           }
+       }
+
     }
+}
